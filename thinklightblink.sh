d5978d6d58992124909e3333bd49766ed699743b
Denis <deniskhoshaba@gmail.com>
2016-10-15 23:11:42 +1100

    printf instead of echo and cleanup

--- thinklightblink.sh
+++ thinklightblink.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
+set -o posix
 cleanup() {
    (printf "off" | sudo tee /proc/acpi/ibm/light) > /dev/null;
 }
  
 control_c() {
-  echo -en "\nThinkLight turned off\n"
   cleanup
   exit $?
 }
@@ -17,9 +16,9 @@ printf "Enter the blink time interval (seconds): " ;
 read "time"
 while(true);
    do (printf "on" | sudo tee /proc/acpi/ibm/light) > /dev/null;
-   sleep $time;
+   sleep "$time";
    (printf "off" | sudo tee /proc/acpi/ibm/light) > /dev/null;
-   sleep $time;
+   sleep "$time";
 done;
 
 (printf: "off " | sudo tee /proc/acpi/ibm/light) > /dev/null;
